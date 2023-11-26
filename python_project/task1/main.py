8d8f05992f6b36f5f1aded19c0c66489890d723b
Fedor Pozdnyakov <fedirit@yandex.ru>
2023-11-26 20:19:56 +0300

    Надежда на то, что выполнил задание 1 и 2, непонятное условие

--- python_project/task1/main.py
+++ python_project/task1/main.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_hw_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -24,10 +33,28 @@ class Mentor:
             return 'Ошибка'
 
 class Lecturer(Mentor)
-    pass
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
 
 class Reviewer(Mentor)
-    pass
+
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
